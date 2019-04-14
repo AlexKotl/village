3ecf7b16aa40e5ac4cba740b98b082ef48c28274
Alex <slicer256@gmail.com>
2019-04-14 22:49:42 +0300

    player refactored into separate class

---
+++ src/objects/player.js
@@ -0,0 +1,76 @@
+//import Phaser from 'phaser';
+
+export default class Player extends Phaser.GameObjects.Sprite {
+    constructor(config) {
+        super(config.scene, config.x, config.y, config.key);
+        config.scene.physics.world.enable(this);
+        config.scene.add.existing(this);
+        
+        this.body.setSize(26, 26, 10, 12);
+        
+        // animations
+        config.scene.anims.create({
+            key: 'left',
+            frames: config.scene.anims.generateFrameNumbers('player', { start: 0, end: 4 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        config.scene.anims.create({
+            key: 'right',
+            frames: config.scene.anims.generateFrameNumbers('player', { start: 5, end: 9 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        config.scene.anims.create({
+            key: 'up',
+            frames: config.scene.anims.generateFrameNumbers('player', { start: 10, end: 14 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        config.scene.anims.create({
+            key: 'down',
+            frames: config.scene.anims.generateFrameNumbers('player', { start: 15, end: 19 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+    
+    update() {
+        this.body.setVelocity(0);
+        
+        if (this.scene.cursors.left.isDown) {
+            this.body.setVelocityX(-100);
+        }
+        else if (this.scene.cursors.right.isDown) {
+            this.body.setVelocityX(100);
+        }
+
+        // Vertical movement
+        if (this.scene.cursors.up.isDown) {
+            this.body.setVelocityY(-100);
+        }
+        else if (this.scene.cursors.down.isDown) {
+            this.body.setVelocityY(100);
+        }
+        
+        if (this.scene.cursors.left.isDown) {
+            this.anims.play('left', true);
+        }
+        else if (this.scene.cursors.right.isDown) {
+            this.anims.play('right', true);
+        }
+        else if (this.scene.cursors.up.isDown) {
+            this.anims.play('up', true);
+        }
+        else if (this.scene.cursors.down.isDown) {
+            this.anims.play('down', true);
+        }
+        else {
+            this.anims.stop();
+        }
+    }
+    
+    setAnimations() {
+        
+    }
+}
--- src/scenes/game-scene.js
+++ src/scenes/game-scene.js
@@ -1,4 +1,6 @@
 import Phaser from 'phaser';
+import Player from '../objects/player'
+//import GameAnimations from '../objects/game-animations'
 
 class GameScene extends Phaser.Scene {
     
@@ -36,33 +38,14 @@ class GameScene extends Phaser.Scene {
         
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 4 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { start: 5, end: 9 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'up',
-            frames: this.anims.generateFrameNumbers('player', { start: 10, end: 14 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'down',
-            frames: this.anims.generateFrameNumbers('player', { start: 15, end: 19 }),
-            frameRate: 10,
-            repeat: -1
-        });
         
-        this.player = this.physics.add.sprite(50, 100, 'player', 1);
-        this.player.body.setSize(26, 26, 10, 12);
+        
+        this.player = new Player({
+            scene: this,
+            x: 50,
+            y: 100,
+            key: 'player',
+        }); 
         
         this.physics.add.collider(this.player, this.layer);
 
@@ -77,49 +60,8 @@ class GameScene extends Phaser.Scene {
     }
     
     update() {
-        
-        // marker.x = layer.getTileX(player.x) * 32;
-        // marker.y = layer.getTileY(player.y) * 32;
-        // 
-        // game.physics.arcade.collide(player, layer);
-        // 
-        this.player.body.setVelocity(0);
-
-        if (this.cursors.left.isDown) {
-            this.player.body.setVelocityX(-100);
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.body.setVelocityX(100);
-        }
-
-        // Vertical movement
-        if (this.cursors.up.isDown) {
-            this.player.body.setVelocityY(-100);
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.body.setVelocityY(100);
-        }
-        
-        if (this.cursors.left.isDown) {
-            this.player.anims.play('left', true);
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.anims.play('right', true);
-        }
-        else if (this.cursors.up.isDown) {
-            this.player.anims.play('up', true);
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.anims.play('down', true);
-        }
-        else {
-            this.player.anims.stop();
-        }
-        
-        // adding to map
-        // if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-        //     map.putTile(3, layer.getTileX(marker.x), layer.getTileY(marker.y));
-        // }
+    
+        this.player.update();
 
     }
 }
