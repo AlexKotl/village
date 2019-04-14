78d62f995ebea3a81cac7e9a4914a65bc194f58b
Alex <slicer256@gmail.com>
2019-04-14 15:12:07 +0300

    final convertion to es6

--- src/scenes/game-scene.js
+++ src/scenes/game-scene.js
@@ -1,8 +1,21 @@
 import Phaser from 'phaser';
 
 class GameScene extends Phaser.Scene {
+    
     constructor() {
         super({ key: 'GameScene' })
+    }
+    
+    generateLevel() {
+        var level = [];
+        for (var i=0; i<50; i++) {
+            level[i] = [];
+            for (var j=0; j<50; j++) {
+                level[i].push((Math.random() > 0.8 ? 14 : 6));
+            }
+        }
+        
+        return level;
     }
 
     preload() {
@@ -14,14 +27,14 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
-        var map = this.make.tilemap({ data: [[1,2], [2,3], [4,1]], tileWidth: 32, tileHeight: 32 });
-        const tiles = map.addTilesetImage('tiles', null, 32, 32);
+        this.map = this.make.tilemap({ data: this.generateLevel(), tileWidth: 32, tileHeight: 32 });
+        const tiles = this.map.addTilesetImage('tiles', null, 32, 32);
         
         // generate map
-        var layer = map.createStaticLayer(0, tiles);
-        map.setCollisionBetween(10, 20);
+        this.layer = this.map.createStaticLayer(0, tiles);
+        this.map.setCollisionBetween(10, 20);
         
-        var cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         this.anims.create({
             key: 'left',
@@ -48,19 +61,66 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         });
         
-        var player = this.physics.add.sprite(50, 100, 'player', 1);
-        player.body.setSize(26, 26, 10, 12);
+        this.player = this.physics.add.sprite(50, 100, 'player', 1);
+        this.player.body.setSize(26, 26, 10, 12);
         
-        this.physics.add.collider(player, layer);
+        this.physics.add.collider(this.player, this.layer);
 
-        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-        this.cameras.main.startFollow(player);
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        this.cameras.main.startFollow(this.player);
         
         // GUI
         var graphics = this.add.graphics();
         graphics.fillStyle(0x000000, 0.5);
         graphics.fillRect(0, 0, 800, 34);
         graphics.setScrollFactor(0);
+    }
+    
+    update() {
+        
+        // marker.x = layer.getTileX(player.x) * 32;
+        // marker.y = layer.getTileY(player.y) * 32;
+        // 
+        // game.physics.arcade.collide(player, layer);
+        // 
+        this.player.body.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.player.body.setVelocityX(-100);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.body.setVelocityX(100);
+        }
+
+        // Vertical movement
+        if (this.cursors.up.isDown) {
+            this.player.body.setVelocityY(-100);
+        }
+        else if (this.cursors.down.isDown) {
+            this.player.body.setVelocityY(100);
+        }
+        
+        if (this.cursors.left.isDown) {
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.anims.play('right', true);
+        }
+        else if (this.cursors.up.isDown) {
+            this.player.anims.play('up', true);
+        }
+        else if (this.cursors.down.isDown) {
+            this.player.anims.play('down', true);
+        }
+        else {
+            this.player.anims.stop();
+        }
+        
+        // adding to map
+        // if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        //     map.putTile(3, layer.getTileX(marker.x), layer.getTileY(marker.y));
+        // }
+
     }
 }
 
