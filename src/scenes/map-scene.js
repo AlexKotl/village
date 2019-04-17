40f3f1971dad1297ada7343e1f96689f881345bd
Alex <slicer256@gmail.com>
2019-04-17 10:41:26 +0300

    noise map generator

--- src/scenes/map-scene.js
+++ src/scenes/map-scene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import SimplexNoise from 'simplex-noise';
 
 export default class MapScene extends Phaser.Scene {
     
@@ -9,15 +10,32 @@ export default class MapScene extends Phaser.Scene {
     preload() {
         this.load.image('tiles', 'assets/tilemaps/tiles.png');
     }
+    
+    generateMap(width, height) {
+        let map = [];
+        const freq = 0.1;
+        const simplex = new SimplexNoise();
+        
+        for (let x=0; x<width; x++) {
+            map[x] = [];
+            for (let y=0; y<height; y++) {
+                let val = simplex.noise2D(x * freq, y * freq) 
+                    + simplex.noise2D(x * freq * 0.3, y * freq * 0.2); // add more details
+                map[x][y] = val > 0 ? 1 : 2;
+            }
+        }
+        console.log(map);
+        return map;
+    }
 
     create() {
-        this.map = this.make.tilemap({ data: [[1,2]], tileWidth: 32, tileHeight: 32 });
+        this.map = this.make.tilemap({ data: this.generateMap(100, 100), tileWidth: 32, tileHeight: 32 });
         const tiles = this.map.addTilesetImage('tiles', null, 32, 32);
         
         // generate map
         this.ground = this.map.createStaticLayer(0, tiles);
         
-        this.cameras.main.zoom = 0.5;
+        this.cameras.main.zoom = 0.3;
         
         const cursors = this.input.keyboard.createCursorKeys();
 
