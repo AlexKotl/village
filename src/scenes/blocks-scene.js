ad1acf81999755052adb122fa9ebf7c7b82dd245
Alex <slicer256@gmail.com>
2019-04-26 00:33:30 +0300

    limit blocks movement

--- src/scenes/blocks-scene.js
+++ src/scenes/blocks-scene.js
@@ -18,7 +18,12 @@ export default class BlocksScene extends Phaser.Scene {
     }
     
     getAllowedMoves(x, y) {
-        
+        return {
+            top: 10,
+            bottom: 100,
+            left: 0,
+            right: 100,
+        }
     }
 
     create() {
@@ -43,13 +48,48 @@ export default class BlocksScene extends Phaser.Scene {
         
         this.input.on('dragstart', (pointer, obj) => {
             console.log('start from ', this.getMapPosition(obj.x, obj.y));
-            
-            //obj.body.moves = false;
+            this.isVerticalMove = undefined;
+            this.draggedFrom = {x: obj.x, y: obj.y};
         });
 
-        this.input.on('drag', function (pointer, obj, dragX, dragY) {
-            obj.setPosition(dragX, obj.y);
-            //console.log(obj);
+        this.input.on('drag', (pointer, obj, dragX, dragY) => {
+            let mapPos = this.getMapPosition(obj.x, obj.y);
+            let allowed = this.getAllowedMoves(mapPos.x, mapPos.y);
+            let dX = dragX - obj.x;
+            let dY = dragY - obj.y;
+            
+            if (this.isVerticalMove === undefined) {
+                this.isVerticalMove = Math.abs(obj.y - dragY) > Math.abs(obj.x - dragX);
+            }
+            
+            if (this.isVerticalMove) {
+                if (dY > 0 && this.draggedFrom.y + allowed.bottom < dragY) { 
+                    obj.setPosition(obj.x, this.draggedFrom.y + allowed.bottom);
+                    console.log('hit bottom')
+                    return true;
+                }
+                if (dY < 0 &&  this.draggedFrom.y - allowed.top > dragY) {
+                    obj.setPosition(obj.x, this.draggedFrom.y - allowed.top);
+                    console.log('hit top')
+                    return true;
+                }
+                obj.setPosition(obj.x, dragY);
+            }
+            else {
+                if (dX > 0 && allowed.right + this.draggedFrom.x < dragX) {
+                    obj.setPosition(this.draggedFrom.x + allowed.right, obj.y);
+                    console.log('hit right')
+                    return true;
+                }
+                if (dX < 0 && this.draggedFrom.x - allowed.left > dragX) {
+                    obj.setPosition(this.draggedFrom.x - allowed.left, obj.y);
+                    console.log('hit left')
+                    return true;
+                }
+                obj.setPosition(dragX, obj.y);
+            }
+            
+            
         });
 
         this.input.on('dragend', (pointer, obj) => {
