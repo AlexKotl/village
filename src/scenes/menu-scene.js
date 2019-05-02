1da99afd7f6490c4165a77f27ca845efd6b7bf2d
Alex <slicer256@gmail.com>
2019-05-02 23:20:11 +0300

    menu animation

--- src/scenes/menu-scene.js
+++ src/scenes/menu-scene.js
@@ -9,10 +9,28 @@ export default class MenuScene extends Phaser.Scene {
         const x = this.game.canvas.width / 2;
         const y = this.game.canvas.height / 2;
         this.add.sprite(x, y, 'cats');
-        this.add.sprite(x, y, 'text');
-        this.add.sprite(x, y, 'menu-mouse');
+        const text = this.add.sprite(x, y, 'text').setAlpha(0);
+        const mouse = this.add.sprite(-x, y, 'menu-mouse');
         
         this.cameras.main.backgroundColor.setTo(255,255,255);
+        
+        // mouse appear
+        this.tweens.add({
+            targets: mouse,
+            x: x,
+            ease: 'Power1',
+            duration: 1000,
+            delay: 500
+        });
+        
+        // title 
+        this.tweens.add({
+            targets: text,
+            alpha: 1,
+            ease: 'Power1',
+            duration: 2000,
+            delay: 1500
+        });
         
         this.input.once('pointerdown', () => {
            this.scene.start('BlocksScene');
